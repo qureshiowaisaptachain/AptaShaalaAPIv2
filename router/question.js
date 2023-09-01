90d9df65a5428f63edd9d254c2cf9cebeb9c7828
qureshiowaisaptachain <qureshi.owais@aptachain.com>
2023-09-01 17:30:39 +0530

    pagination and query

--- router/question.js
+++ router/question.js
@@ -6,17 +6,15 @@ const {
   updateQuestion,
   deleteQuestion,
 } = require('../controller/question');
+const { pagination } = require('../middleware/pagination');
+const Question = require('../model/questions');
 const { protect, authorize } = require('../middleware/authorization');
 
 const router = express.Router();
 router.post('/', protect, authorize(['CreateQuestion']), addQuestion);
-router.get('/', protect, authorize(['ReadQuestion']), getQuestion);
-router.get(
-  '/queryQuestion',
-  protect,
-  authorize(['ReadQuestion']),
-  queryQuestion
-);
+// router.get('/', protect, authorize(['ReadQuestion']), getQuestion);
+router.get('/', protect, authorize(['ReadQuestion']), queryQuestion);
+router.get('/pagination',pagination(Question),protect,authorize(['ReadQuestion']),queryQuestion);
 router.put('/', protect, authorize(['UpdateQuestion']), updateQuestion);
 router.delete('/', protect, authorize(['DeleteQuestion']), deleteQuestion);
 
