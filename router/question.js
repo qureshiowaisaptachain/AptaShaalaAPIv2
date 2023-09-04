15c77469b73675a3cb57857ff47ef195dee926e2
qureshiowaisaptachain <qureshi.owais@aptachain.com>
2023-09-04 13:05:57 +0530

    pagiantion

--- router/question.js
+++ router/question.js
@@ -12,9 +12,14 @@ const { protect, authorize } = require('../middleware/authorization');
 
 const router = express.Router();
 router.post('/', protect, authorize(['CreateQuestion']), addQuestion);
-// router.get('/', protect, authorize(['ReadQuestion']), getQuestion);
 router.get('/', protect, authorize(['ReadQuestion']), queryQuestion);
-router.get('/pagination',pagination(Question),protect,authorize(['ReadQuestion']),queryQuestion);
+router.get(
+  '/pagination',
+  pagination(Question),
+  protect,
+  authorize(['ReadQuestion']),
+  queryQuestion
+);
 router.put('/', protect, authorize(['UpdateQuestion']), updateQuestion);
 router.delete('/', protect, authorize(['DeleteQuestion']), deleteQuestion);
 
